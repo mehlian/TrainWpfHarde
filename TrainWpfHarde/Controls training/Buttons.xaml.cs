c94ee421c305197aeda0eff06f2b7b514e6d7802
mehlian <xpilarz0@gmail.com>
2017-09-14 10:37:24 +0200

    Added buttons control

---
+++ TrainWpfHarde/Controls training/Buttons.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for Buttons.xaml
+    /// </summary>
+    public partial class Buttons : UserControl
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            button1.Foreground = new SolidColorBrush(Colors.Blue);
+            MessageBox.Show("On Hover click event occurs.");
+        }
+
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+            button2.Foreground = new SolidColorBrush(Colors.Green);
+            MessageBox.Show("On Press click event occurs.");
+        }
+
+        void OnClick3(object sender, RoutedEventArgs e)
+        {
+            button1.Foreground = new SolidColorBrush(Colors.Green);
+            button2.Foreground = new SolidColorBrush(Colors.Blue);
+            MessageBox.Show("On Release click event occurs.");
+        }
+    }
+}
--- TrainWpfHarde/MainWindow.xaml.cs
+++ TrainWpfHarde/MainWindow.xaml.cs
@@ -24,24 +24,5 @@ namespace TrainWpfHarde
         {
             InitializeComponent();
         }
-
-        void OnClick1(object sender,RoutedEventArgs e)
-        {
-            button1.Foreground = new SolidColorBrush(Colors.Blue);
-            MessageBox.Show("On Hover click event occurs.");
-        }
-
-        void OnClick2(object sender, RoutedEventArgs e)
-        {
-            button2.Foreground = new SolidColorBrush(Colors.Green);
-            MessageBox.Show("On Press click event occurs.");
-        }
-
-        void OnClick3(object sender, RoutedEventArgs e)
-        {
-            button1.Foreground = new SolidColorBrush(Colors.Green);
-            button2.Foreground = new SolidColorBrush(Colors.Blue);
-            MessageBox.Show("On Release click event occurs.");
-        }
     }
 }
