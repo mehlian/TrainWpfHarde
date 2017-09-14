66eb3ef206135586a91e818efa9ccc9e6a05195b
mehlian <xpilarz0@gmail.com>
2017-09-14 21:45:52 +0200

    added comboboxes and contextmenus

---
+++ TrainWpfHarde/Controls training/ComboBoxes.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for ComboBoxes.xaml
+    /// </summary>
+    public partial class ComboBoxes : UserControl
+    {
+        public ComboBoxes()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectionChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            var item = (ComboBoxItem)comboBox.SelectedItem;
+            Status_Text.Text = item.Content.ToString();
+        }
+    }
+}
---
+++ TrainWpfHarde/Controls training/ContextMenus.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for ContextMenus.xaml
+    /// </summary>
+    public partial class ContextMenus : UserControl
+    {
+        public ContextMenus()
+        {
+            InitializeComponent();
+        }
+
+        private void Bold_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox1.FontWeight = FontWeights.Bold;
+        }
+
+        private void Bold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox1.FontWeight = FontWeights.Normal;
+        }
+
+        private void Italic_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox1.FontStyle = FontStyles.Italic;
+        }
+
+        private void Italic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox1.FontStyle = FontStyles.Normal;
+        }
+
+        private void IncreaseFont_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.FontSize < 18)
+                textBox1.FontSize += 2;
+        }
+
+        private void DecreaseFont_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.FontSize > 10)
+                textBox1.FontSize -= 2;
+        }
+
+    }
+}
