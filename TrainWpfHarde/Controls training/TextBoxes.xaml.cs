1e00f4fe001108015ef3a28e885280d3a61f9522
mehlian <xpilarz0@gmail.com>
2017-09-17 19:41:51 +0200

    added tooglebox, textbox

---
+++ TrainWpfHarde/Controls training/TextBoxes.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for TextBoxes.xaml
+    /// </summary>
+    public partial class TextBoxes : UserControl
+    {
+        private string[] selectionItems = new string[] { "FireBird", "Asswe", "Ssak", "Kij" };
+        public TextBoxes()
+        {
+            InitializeComponent();
+
+        }
+
+        //TODO: Sugestie podczas pisania
+        //TODO: Placeholder
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var autoSuggestBox = (TextBox)sender;
+            var filter = selectionItems.Where(p => p.StartsWith(autoSuggestBox.Text)).ToArray();
+            autoSuggestBox.Text = filter[0];
+        }
+    }
+}
