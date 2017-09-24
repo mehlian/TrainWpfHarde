5d48aa2182f50efec5720cda5b7f03224e37405d
mehlian <xpilarz0@gmail.com>
2017-09-24 14:51:03 +0200

    Added GridViews and AppMenus

---
+++ TrainWpfHarde/Controls training/AppMenus.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for AppMenus.xaml
+    /// </summary>
+    public partial class AppMenus : UserControl
+    {
+        public AppMenus()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            Status_Text.Text = "File: " + item.Header;
+        }
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            Status_Text.Text = "Edit: " + item.Header;
+        }
+    }
+}
---
+++ TrainWpfHarde/Controls training/GridViews.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for GridViews.xaml
+    /// </summary>
+    public partial class GridViews : UserControl
+    {
+        public GridViews()
+        {
+            InitializeComponent();
+            MenList.Items.Add(new Person() { Name = "Ali", ID = "123A", Age = 20 });
+            MenList.Items.Add(new Person() { Name = "Akram", ID = "456X", Age = 35 });
+            MenList.Items.Add(new Person() { Name = "Salman", ID = "333E", Age = 49 });
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+    }
+}
