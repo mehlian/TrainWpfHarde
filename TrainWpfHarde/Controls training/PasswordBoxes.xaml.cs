57c8218bd02d0d6002de06d01431e3cf3591c23b
mehlian <xpilarz0@gmail.com>
2017-09-25 13:58:07 +0200

    Added Canvas control, passBox, Custom Resources Examples with Dictionary

---
+++ TrainWpfHarde/Controls training/PasswordBoxes.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainWpfHarde
+{
+    /// <summary>
+    /// Interaction logic for PasswordBoxes.xaml
+    /// </summary>
+    public partial class PasswordBoxes : UserControl
+    {
+        public PasswordBoxes()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(pwBox.Password=="123456")
+            {
+                Status_Text.Text = "Correct Pass";
+            }
+            else
+            {
+                Status_Text.Text = "Wrong Pass";
+            }
+        }
+    }
+}
